9599502ad8d90e9a4399441fb01a37ee24678a92
Guilherme Gps <guilherme.santos@eigmercados.com.br>
2020-01-11 18:34:07 -0300

    Criando formatação para os campos da tela de DARF FIIs

--- js/darfFiisPage.js
+++ js/darfFiisPage.js
@@ -2,8 +2,4 @@
     "use strict"; // Start of use strict
 
     $("#loadPMeLucroFIIs").load("page/component/PMeLucroFIIs.html");
-
-    // $('#contato').click(function(){
-    //     $("#loadPage").load("page/contato.html");
-    // });
 })(jQuery); // End of use strict
