bd12fa1f508200276277e9db97ada33275f9962e
Guilherme Gps <guilherme.santos@eigmercados.com.br>
2020-01-11 11:44:35 -0300

    Criação do form para o calculo de DARF de FIIs

---
+++ js/darfFiisPage.js
@@ -0,0 +1,9 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    $("#loadPMeLucroFIIs").load("page/component/PMeLucroFIIs.html");
+
+    // $('#contato').click(function(){
+    //     $("#loadPage").load("page/contato.html");
+    // });
+})(jQuery); // End of use strict
